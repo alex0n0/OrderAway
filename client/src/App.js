dfe4291cb5b890e584a00fd10db0c26a1102b867
alex0n0 <42526086+alex0n0@users.noreply.github.com>
2019-12-25 11:07:44 +1100

    testing proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestingProxy from './components/experimental/testingproxy';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <TestingProxy />
       </header>
     </div>
   );
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 
-app.get('/', function(req, res) {
-    res.send('<h1>hello world</h1>');
+app.get("/", function(req, res) {
+    res.send("<h1>hello world</h1>");
+});
+
+app.get("/testingproxy", function(req, res) {
+    res.json({"message":"proxy was successful"});
+    // res.status(404).end(); // not found
+    // res.status(400).end(); // bad request
 });
 
 const PORT = process.env.PORT || 5000;
