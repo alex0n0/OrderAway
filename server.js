86a786edea62634c70026221ed4b74d59e167878
aaa_alex <alexwu7583@gmail.com>
2020-01-09 19:21:31 +1100

    mongo adjustements

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ require("./api-routes")(app);
 
 
 // mongoose.connect("mongodb://localhost/zzzbcsproj3", { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect("mongodb://heroku_2ll1xfxt:jaoi32tLo9>@ds047468.mlab.com:47468/heroku_2ll1xfxt", { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://heroku_2ll1xfxt:ul869hll9r420tu5cr89olql87@ds047468.mlab.com:47468/heroku_2ll1xfxt", { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
