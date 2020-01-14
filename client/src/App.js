90d4a32efa82fb6431c5b30755b4bfc0bac78f2f
aaa_alex <alexwu7583@gmail.com>
2020-01-15 09:47:56 +1100

    db seeds update

--- client/src/App.js
+++ client/src/App.js
@@ -41,13 +41,13 @@ class App extends React.Component {
 
                 <div className="row justify-content-center px-3">
                   <div className="col-12 col-sm-6 border border-bottom-0 py-3">
-                    <Link to="/signup" target="_blank">sign up</Link>
+                    <Link to="/signup">sign up</Link>
                   </div>
                 </div>
 
                 <div className="row justify-content-center px-3">
                   <div className="col-12 col-sm-6 border py-3">
-                    <Link to="/signin" target="_blank">sign in</Link>
+                    <Link to="/signin">sign in</Link>
                   </div>
                 </div>
 
